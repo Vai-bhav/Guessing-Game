75034745b3ae3678757c6746d79d7fd5e431e52d
Vaibhav Kumar <vaibhavkumar.jnva@gmail.com>
2019-03-23 12:21:53 +0530

    First Commit

---
+++ app/src/main/java/com/example/guessinggame/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.guessinggame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int randomNumber;
+    public void generateRandomNumber(){
+        Random rand = new Random();
+
+        randomNumber = rand.nextInt(20) + 1;
+    }
+
+    public void guess(View view){
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        int guessValue = Integer.parseInt(editText.getText().toString());
+        String message;
+
+        if(guessValue > randomNumber){
+            message ="Lower";
+        }
+        else if(guessValue < randomNumber){
+            message ="Higher";
+        }
+        else{
+            message = "Yeah you got it";
+
+            generateRandomNumber();
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        Log.i("Entered Value",editText.getText().toString());
+
+        Log.i("Random Number",Integer.toString(randomNumber));
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        generateRandomNumber();
+
+    }
+}
